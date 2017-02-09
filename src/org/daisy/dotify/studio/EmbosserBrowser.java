75f68a987fee08829ee1ff1edd6f6cd049946f2a
Joel Håkansson <joel.hakansson@mtm.se>
2017-02-09 10:54:50 +0100

    Adds support for opening several files from the JavaFx menu

---
+++ src/org/daisy/dotify/studio/EmbosserBrowser.java
@@ -0,0 +1,45 @@
+package org.daisy.dotify.studio;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.googlecode.e2u.Start;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class EmbosserBrowser extends VBox {
+	private WebView browser;
+
+	public EmbosserBrowser(String[] args) {
+
+        String url = getClass().getResource("resource-files/fail.html").toString();
+        try {
+			url = Start.run(args, false);
+		} catch (Exception e1) {
+			Logger.getLogger(this.getClass().getCanonicalName()).log(Level.SEVERE, "Failed to load server.", e1);;
+		}
+        browser = new WebView();
+
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load(url);
+        webEngine.setCreatePopupHandler(p-> {
+                Stage stage = new Stage(StageStyle.UTILITY);
+                WebView wv2 = new WebView();
+                stage.setScene(new Scene(wv2));
+                stage.show();
+                return wv2.getEngine();
+            }
+        );
+        getChildren().addAll(browser);
+	}
+	
+	public void reload() {
+		browser.getEngine().reload();
+	}
+
+}
--- src/org/daisy/dotify/studio/MainFx.java
+++ src/org/daisy/dotify/studio/MainFx.java
@@ -1,22 +1,24 @@
 package org.daisy.dotify.studio;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.util.Optional;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
-
-import com.googlecode.e2u.Start;
  
 public class MainFx extends Application {
+	private TabPane tabPane;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -24,34 +26,73 @@ public class MainFx extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Dotify Studio");
+        
         VBox root = new VBox();
         Scene scene = new Scene(root, 800, 450);
-        String url = getClass().getResource("resource-files/fail.html").toString();
-        try {
-			url = Start.run(getParameters().getRaw().toArray(new String[]{}), false);
-		} catch (Exception e1) {
-			Logger.getLogger(this.getClass().getCanonicalName()).log(Level.SEVERE, "Failed to load server.", e1);;
-		}
-        WebView browser = new WebView();
-        WebEngine webEngine = browser.getEngine();
-        webEngine.load(url);
-        ((VBox)scene.getRoot()).getChildren().addAll(makeMenu(), browser);
+
+        tabPane = new TabPane();
+
+        
+        String[] args = getParameters().getRaw().toArray(new String[]{});
+        if (args.length>0) {
+        	addTab(null, args);
+        }
+
+        ((VBox)scene.getRoot()).getChildren().addAll(makeMenu(primaryStage), tabPane);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     
-    private static MenuBar makeMenu() {
+    private MenuBar makeMenu(Stage stage) {
     	MenuBar menuBar = new MenuBar();
 		Menu menuFile = new Menu("File");
 		MenuItem open = new MenuItem("Open...");
 		open.setAccelerator(KeyCombination.keyCombination("CTRL+O"));
-		open.setOnAction(e->System.out.println("Open"));
+		open.setOnAction(e->showOpenDialog(stage));
 		MenuItem saveAs = new MenuItem("Save...");
 		MenuItem importItem = new MenuItem("Import...");
 		menuFile.getItems().addAll(open, saveAs, importItem);
 		Menu menuEdit = new Menu("Edit");
+		MenuItem refresh = new MenuItem("Refresh");
+		refresh.setAccelerator(KeyCombination.keyCombination("F5"));
+
+		refresh.setOnAction(e-> {
+			Tab t = tabPane.getSelectionModel().getSelectedItem();
+			if (t!=null) {
+				((EmbosserBrowser)t.getContent()).reload();
+			}
+		});
+
+		menuEdit.getItems().addAll(refresh);
 		Menu menuView = new Menu("View");
 		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
 		return menuBar;
+    }
+    
+    private void showOpenDialog(Stage stage) {
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Open Resource File");
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("PEF-files", "*.pef"));
+    	File selected = fileChooser.showOpenDialog(stage);
+    	if (selected!=null) {
+    		addTab(selected);
+    	}
+    }
+    
+    private void addTab(File f) { 
+    	addTab(f.getName(), new String[]{"-open", f.getAbsolutePath()});
+    }
+    
+    private void addTab(String title, String[] args) {
+        Tab tab = new Tab();
+        if (title==null && args.length>=2) {
+        	title = args[1];
+        }
+        if (title!=null) {
+        	tab.setText(title);
+        }
+        tab.setContent(new EmbosserBrowser(args));
+        tabPane.getTabs().add(tab);
+        tabPane.getSelectionModel().select(tab);
     }
 }
