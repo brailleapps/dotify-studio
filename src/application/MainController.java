74313c9fa5c3204fbcad3de4593fcfa3d79d3bfe
Joel Håkansson <joel.hakansson@mtm.se>
2017-03-15 11:59:34 +0100

    Adds fxml for Main

---
+++ src/application/MainController.java
@@ -0,0 +1,125 @@
+package application;
+
+import java.io.File;
+
+import application.about.AboutView;
+import application.l10n.Messages;
+import application.prefs.PreferencesView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Modality;
+import javafx.stage.Window;
+
+public class MainController {
+	@FXML private BorderPane root;
+	@FXML private TabPane tabPane;
+
+	void openArgs(String[] args) {
+        if (args.length>0) {
+        	addTab(null, args);
+        }
+	}
+/*
+    private MenuBar makeMenu(Window stage) {
+    	MenuBar menuBar = new MenuBar();
+		Menu menuFile = new Menu("File");
+		MenuItem open = new MenuItem("Open...");
+		//open.setAccelerator(KeyCombination.keyCombination("shortcut+O"));
+		//open.setOnAction(e->showOpenDialog(stage));
+		MenuItem saveAs = new MenuItem("Save...");
+		MenuItem importItem = new MenuItem("Import...");
+		menuFile.getItems().addAll(open, saveAs, importItem);
+		Menu menuEdit = new Menu("Edit");
+		MenuItem refresh = new MenuItem("Refresh");
+		refresh.setAccelerator(KeyCombination.keyCombination("F5"));
+			refresh.setOnAction(e-> {refresh();
+		});
+
+		menuEdit.getItems().addAll(refresh);
+		Menu menuView = new Menu("View");
+		Menu menuWindow = new Menu("Window");
+		MenuItem preferences = new MenuItem(Messages.PREFERENCES_MENU_ITEM.localize());
+		preferences.setOnAction(e -> {
+			openPreferences();
+		});
+		menuWindow.getItems().addAll(preferences);
+		Menu menuHelp = new Menu("Help");
+		MenuItem about = new MenuItem("About");
+		about.setOnAction(e -> {
+			openAbout();
+		});
+		menuHelp.getItems().addAll(about);
+		menuBar.getMenus().addAll(menuFile, menuEdit, menuView, menuWindow, menuHelp);
+		return menuBar;
+    }
+  */  
+    @FXML
+    public void refresh() {
+		Tab t = tabPane.getSelectionModel().getSelectedItem();
+		if (t!=null) {
+			((EmbosserBrowser)t.getContent()).reload();
+		}
+    }
+    
+    @FXML
+    public void closeTab() {
+    	Tab t = tabPane.getSelectionModel().getSelectedItem();
+		if (t!=null) {
+			tabPane.getTabs().remove(t);
+		}
+    }
+    
+    @FXML
+    public void openPreferences() {
+		PreferencesView dialog = new PreferencesView();
+		dialog.initOwner(root.getScene().getWindow());
+		dialog.initModality(Modality.APPLICATION_MODAL);
+		dialog.showAndWait();
+    }
+    
+    @FXML
+    public void openAbout() {
+		AboutView dialog = new AboutView();
+		dialog.initOwner(root.getScene().getWindow());
+		dialog.initModality(Modality.APPLICATION_MODAL); 
+		dialog.showAndWait();
+    }
+    
+    @FXML
+    public void showOpenDialog() {
+    	Window stage = root.getScene().getWindow();
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Open Resource File");
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("PEF-files", "*.pef"));
+    	File selected = fileChooser.showOpenDialog(stage);
+    	if (selected!=null) {
+    		addTab(selected);
+    	}
+    }
+    
+    private void addTab(File f) { 
+    	addTab(f.getName(), new String[]{"-open", f.getAbsolutePath()});
+    }
+    
+    private void addTab(String title, String[] args) {
+        Tab tab = new Tab();
+        if (title==null && args.length>=2) {
+        	title = args[1];
+        }
+        if (title!=null) {
+        	tab.setText(title);
+        }
+        tab.setContent(new EmbosserBrowser(args));
+        tabPane.getTabs().add(tab);
+        tabPane.getSelectionModel().select(tab);
+    }
+
+}
--- src/application/MainFx.java
+++ src/application/MainFx.java
@@ -1,31 +1,18 @@
 package application;
 
-import java.io.File;
 import java.io.IOException;
 
-import application.about.AboutView;
 import application.l10n.Messages;
-import application.prefs.PreferencesView;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Modality;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
  
 public class MainFx extends Application {
-	private TabPane tabPane;
 
     public static void main(String[] args) {
         launch(args);
@@ -43,91 +30,15 @@ public class MainFx extends Application {
         primaryStage.setWidth(bounds.getWidth());
         primaryStage.setHeight(bounds.getHeight());
         
-        BorderPane root = new BorderPane();
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Main.fxml"), Messages.getBundle());
+        Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
-
-        tabPane = new TabPane();
         
-        String[] args = getParameters().getRaw().toArray(new String[]{});
-        if (args.length>0) {
-        	addTab(null, args);
-        }
-
-        root.setTop(makeMenu(primaryStage));
-        root.setCenter(tabPane);
+        MainController controller = fxmlLoader.<MainController>getController();
+        controller.openArgs(getParameters().getRaw().toArray(new String[]{}));
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
-    private MenuBar makeMenu(Stage stage) {
-    	MenuBar menuBar = new MenuBar();
-		Menu menuFile = new Menu("File");
-		MenuItem open = new MenuItem("Open...");
-		open.setAccelerator(KeyCombination.keyCombination("shortcut+O"));
-		open.setOnAction(e->showOpenDialog(stage));
-		MenuItem saveAs = new MenuItem("Save...");
-		MenuItem importItem = new MenuItem("Import...");
-		menuFile.getItems().addAll(open, saveAs, importItem);
-		Menu menuEdit = new Menu("Edit");
-		MenuItem refresh = new MenuItem("Refresh");
-		refresh.setAccelerator(KeyCombination.keyCombination("F5"));
 
-		refresh.setOnAction(e-> {
-			Tab t = tabPane.getSelectionModel().getSelectedItem();
-			if (t!=null) {
-				((EmbosserBrowser)t.getContent()).reload();
-			}
-		});
-
-		menuEdit.getItems().addAll(refresh);
-		Menu menuView = new Menu("View");
-		Menu menuWindow = new Menu("Window");
-		MenuItem preferences = new MenuItem(Messages.PREFERENCES_MENU_ITEM.localize());
-		preferences.setOnAction(e -> {
-			PreferencesView dialog = new PreferencesView();
-			dialog.initOwner(stage);
-			dialog.initModality(Modality.APPLICATION_MODAL);
-			dialog.showAndWait();
-		});
-		menuWindow.getItems().addAll(preferences);
-		Menu menuHelp = new Menu("Help");
-		MenuItem about = new MenuItem("About");
-		about.setOnAction(e -> {
-			AboutView dialog = new AboutView();
-			dialog.initOwner(stage);
-			dialog.initModality(Modality.APPLICATION_MODAL); 
-			dialog.showAndWait();
-		});
-		menuHelp.getItems().addAll(about);
-		menuBar.getMenus().addAll(menuFile, menuEdit, menuView, menuWindow, menuHelp);
-		return menuBar;
-    }
-    
-    private void showOpenDialog(Stage stage) {
-    	FileChooser fileChooser = new FileChooser();
-    	fileChooser.setTitle("Open Resource File");
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("PEF-files", "*.pef"));
-    	File selected = fileChooser.showOpenDialog(stage);
-    	if (selected!=null) {
-    		addTab(selected);
-    	}
-    }
-    
-    private void addTab(File f) { 
-    	addTab(f.getName(), new String[]{"-open", f.getAbsolutePath()});
-    }
-    
-    private void addTab(String title, String[] args) {
-        Tab tab = new Tab();
-        if (title==null && args.length>=2) {
-        	title = args[1];
-        }
-        if (title!=null) {
-        	tab.setText(title);
-        }
-        tab.setContent(new EmbosserBrowser(args));
-        tabPane.getTabs().add(tab);
-        tabPane.getSelectionModel().select(tab);
-    }
 }
