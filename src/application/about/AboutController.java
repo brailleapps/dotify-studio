f846d4c32e88343ace3a6b5d0413f30e42d1cd05
Joel Håkansson <joel.hakansson@mtm.se>
2017-04-19 16:07:59 +0200

    Improves about dialog

--- src/application/about/AboutController.java
+++ src/application/about/AboutController.java
@@ -18,17 +18,19 @@ import javafx.stage.Stage;
 
 public class AboutController {
 	private static final String PEF_URL = "http://pef-format.org";
+	private static final String BRAILLE_APPS_URL = "https://github.com/brailleapps";
 	@FXML private Label title;
 	@FXML private Label description;
 	@FXML private Label version;
-	@FXML private Text moreAboutPef;
 	@FXML private Hyperlink pefLink;
+	@FXML private Hyperlink contributeLink;
 	@FXML private Button ok;
 
 	@FXML
 	public void initialize() {
 		version.setText(Messages.APPLICATION_VERSION.localize(BuildInfo.VERSION, BuildInfo.BUILD));
 		pefLink.setText(PEF_URL);
+		contributeLink.setText(BRAILLE_APPS_URL);
 	}
 	
 	@FXML
@@ -38,12 +40,23 @@ public class AboutController {
 	
 	@FXML
 	public void visitPefFormat() {
+		visit(PEF_URL);
+	}
+	
+	@FXML
+	public void visitGithub() {
+		visit(BRAILLE_APPS_URL);
+	}
+	
+	
+	private void visit(String url) {
 		if (Desktop.isDesktopSupported()) {
 			try {
-				Desktop.getDesktop().browse(new URI(PEF_URL));
+				Desktop.getDesktop().browse(new URI(url));
 			} catch (IOException | URISyntaxException e) {
 
 			}
 		}
+
 	}
 }
