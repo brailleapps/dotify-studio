ef9abe8b45e0ac54f5ae96caa9c798f666905e5d
Joel Håkansson <joel.hakansson@mtm.se>
2017-04-20 13:30:06 +0200

    Removes unused code

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -177,40 +177,7 @@ public class MainController {
         	addTab(null, args);
         }
 	}
-/*
-    private MenuBar makeMenu(Window stage) {
-    	MenuBar menuBar = new MenuBar();
-		Menu menuFile = new Menu("File");
-		MenuItem open = new MenuItem("Open...");
-		//open.setAccelerator(KeyCombination.keyCombination("shortcut+O"));
-		//open.setOnAction(e->showOpenDialog(stage));
-		MenuItem saveAs = new MenuItem("Save...");
-		MenuItem importItem = new MenuItem("Import...");
-		menuFile.getItems().addAll(open, saveAs, importItem);
-		Menu menuEdit = new Menu("Edit");
-		MenuItem refresh = new MenuItem("Refresh");
-		refresh.setAccelerator(KeyCombination.keyCombination("F5"));
-			refresh.setOnAction(e-> {refresh();
-		});
 
-		menuEdit.getItems().addAll(refresh);
-		Menu menuView = new Menu("View");
-		Menu menuWindow = new Menu("Window");
-		MenuItem preferences = new MenuItem(Messages.PREFERENCES_MENU_ITEM.localize());
-		preferences.setOnAction(e -> {
-			openPreferences();
-		});
-		menuWindow.getItems().addAll(preferences);
-		Menu menuHelp = new Menu("Help");
-		MenuItem about = new MenuItem("About");
-		about.setOnAction(e -> {
-			openAbout();
-		});
-		menuHelp.getItems().addAll(about);
-		menuBar.getMenus().addAll(menuFile, menuEdit, menuView, menuWindow, menuHelp);
-		return menuBar;
-    }
-  */  
     @FXML
     public void refresh() {
 		Tab t = tabPane.getSelectionModel().getSelectedItem();
