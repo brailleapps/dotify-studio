972afd509ce1b126985df2163008f22ac1459bc2
Joel Håkansson <joel.hakansson@mtm.se>
2018-02-26 10:49:22 +0100

    Uses manifest retriever

--- src/application/common/BuildInfo.java
+++ src/application/common/BuildInfo.java
@@ -1,46 +1,28 @@
 package application.common;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
+import org.daisy.dotify.common.java.ManifestRetriever;
 
-//TODO: move ManifestRetriever from dotify-cli to common and use that
+/**
+ * Provides common properties for the system
+ * @author Joel Håkansson
+ */
 public class BuildInfo {
-	public final static String NAME;
-	public final static String VERSION;
-	public final static String BUILD;
-	static {
-		Class<BuildInfo> clazz = BuildInfo.class;
-		String className = clazz.getSimpleName() + ".class";
-		String classPath = clazz.getResource(className).toString();
-		boolean failed = false;
-		Attributes attr = null;
-		if (!classPath.startsWith("jar")) {
-		  // Class not from JAR
-			failed = true;
-		} else {
-			String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + 
-			    "/META-INF/MANIFEST.MF";
-			Manifest manifest;
-			try {
-				manifest = new Manifest(new URL(manifestPath).openStream());
-				attr = manifest.getMainAttributes();
-			} catch (MalformedURLException e) {
-				failed = true;
-			} catch (IOException e) {
-				failed = true;
-			}
-		}
-		if (failed || attr == null) {
-			NAME = "Dotify Studio";
-			BUILD = "N/A";
-			VERSION = "N/A";
-		} else {
-			NAME = attr.getValue("Implementation-Title");
-			VERSION = attr.getValue("Implementation-Version");
-			BUILD = attr.getValue("Repository-Revision");
-		}
+	private static final ManifestRetriever retriever = new ManifestRetriever(BuildInfo.class);
+	/**
+	 * Defines the system name
+	 */
+	public final static String NAME = getWithDefault(retriever.getManifest().getMainAttributes().getValue("Implementation-Title"), "Dotify Studio");
+	/**
+	 * Defines the system release
+	 */
+	public final static String VERSION = getWithDefault(retriever.getManifest().getMainAttributes().getValue("Implementation-Version"), "N/A");
+	/**
+	 * Defines the system build
+	 */
+	public final static String BUILD = getWithDefault(retriever.getManifest().getMainAttributes().getValue("Repository-Revision"), "N/A");
+	
+	private static final <T> T getWithDefault(T val, T def) {
+		return (val!=null?val:def);
 	}
+	
 }
