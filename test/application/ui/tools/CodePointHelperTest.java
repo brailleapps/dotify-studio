ebb75539d6d0415ab89a5ed2ce5bcd5086bc4ee3
Joel Håkansson <joel.hakansson@pm.me>
2019-03-19 08:50:23 +0100

    Add tests from the devtools project

--- test/application/ui/tools/CodePointHelperTest.java
+++ test/application/ui/tools/CodePointHelperTest.java
@@ -5,12 +5,30 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import application.ui.tools.CodePointHelper.Mode;
+import application.ui.tools.CodePointHelper.Style;
 
 public class CodePointHelperTest {
-
+	
 	@Test
-	public void testParse() {
+	public void testParse_01() {
+		String input = "0065,0066,0067";
+		String expected =  "ABC";
+		String actual = CodePointHelper.parse(input, Mode.DECIMAL);
+		org.junit.Assert.assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testParse_02() {
 		String actual = CodePointHelper.parse("32 | test\n33 | comment", Mode.DECIMAL);
 		assertEquals(" !", actual);
+	}
+	
+	@Test
+	public void testFormat() {
+		String input = "ABC";
+		String expected = "65, 66, 67";
+		String actual = CodePointHelper.format(input, Style.COMMA, Mode.DECIMAL);
+		org.junit.Assert.assertEquals(expected, actual);
 	}
 }
